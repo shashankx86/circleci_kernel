771c97d71c544890a27b82e008a5b9aafe508af7
MD Raza <raza231198@gmail.com>
2020-10-14 15:59:43 +0530

    Update build.sh
    [skip ci]

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 ZIP_DIR=$KERNEL_DIR/AnyKernel3
 CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
-CONFIG=msm8937-perf_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 CROSS_COMPILE+="ccache "
