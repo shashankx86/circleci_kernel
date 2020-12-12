09fe02aa265fa039a6b50401c36cfaa66b44c11d
MD Raza <raza231198@gmail.com>
2020-12-12 06:06:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -40,27 +40,8 @@ echo -e "\n(i) Cloning ToolChain..."
 git clone https://github.com/raza231198/aarch64-linux-android-4.9 aarch64-linux-android-4.9
 
 echo -e "\n(i) Staring Build..."
-make  O=out $CONFIG $THREAD
-make  O=out $THREAD
-
-echo -e "\n(i) Cloning AnyKernel3..."
-git clone https://github.com/raza231198/AnyKernel3 AnyKernel3
-
-if [ $(find "${OUTDIR}" -name 'wlan.ko') ]; then
-	echo -e "\n(i) Strip and move modules to AnyKernel3..."
-	# thanks to @adekmaulana
-	for MOD in $(find "${OUTDIR}" -name 'wlan.ko') ; do
-		"${STRIP}" --strip-unneeded --strip-debug "${MOD}" &> /dev/null
-		"${SRCDIR}"/scripts/sign-file sha512 \
-		"${OUTDIR}/signing_key.priv" \
-		"${OUTDIR}/signing_key.x509" \
-		"${MOD}"
-	find "${OUTDIR}" -name 'wlan.ko' -exec cp {} "${MODULEDIR}" \;
-		done
-	echo -e "\n(i) Done moving modules"
-fi
-echo -e "\n(i) Making ZiP"
-cd $ZIP_DIR
-cp $KERN_IMG $ZIP_DIR
+make  O=out $CONFIG savedefconfig
+mkdir $ZIP_DIR
+cp out/defconfig $ZIP_DIR/defconfig
 make normal &>/dev/null
 ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -n "Latest Kernel Release" -b "Kernel $(echo $VERSION)" -c ${CIRCLE_SHA1} -delete ${VERSION} ${ZIP_DIR}
